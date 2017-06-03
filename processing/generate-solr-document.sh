26d027eba0786f83fd432e8cac414fb660474c25
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-06-03 12:25:54 +0100

    Updates to the shell scripts for indexing the MSS

--- processing/generate-solr-document.sh
+++ processing/generate-solr-document.sh
@@ -4,13 +4,13 @@
 # $1 = xQuery file
 # $2 = Output file
 # $3 = Solr `type` (for deleting only one type of record)
+# $4 = Solr address for indexing
 
-echo `pwd`
 echo "Generating Solr XML document"
 java -cp "saxon/saxon9he.jar" net.sf.saxon.Query -q:$1 | xmllint --format - > $2
 
 echo "Emptying Solr"
-curl "http://172.28.128.10:8983/solr/tolkien-catalogue/update?stream.body=<delete><query>type:${3}</query></delete>&commit=true"
+curl "http://${4}:8983/solr/tolkien-catalogue/update?stream.body=<delete><query>type:${3}</query></delete>&commit=true"
 
 echo "Reindexing in Solr"
-curl "http://172.28.128.10:8983/solr/tolkien-catalogue/update?commit=true" --data-binary @$2 -H "Content-Type: text/xml"
+curl "http://${4}:8983/solr/tolkien-catalogue/update?commit=true" --data-binary @$2 -H "Content-Type: text/xml"
