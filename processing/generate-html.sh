bf15ed1b7f72ce0367e6d1d1dd0cda825dd8818a
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-08-01 09:54:59 +0100

    Fixed: Update HTML generation script
    Added a 'tidy' step after generating the HTML

--- processing/generate-html.sh
+++ processing/generate-html.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 java -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:main -xsl:convert2HTML.xsl
+
+echo "Prettifying output"
+#find ./html/ -name "*.html" -type f -exec xmllint --output '{}' --format '{}' \;
+find ./html/ -name "*.html" -type f -exec tidy --doctype omit --numeric-entities yes --tidy-mark no -i -m -w 160 -asxhtml -utf8 -output '{}' '{}' \;
