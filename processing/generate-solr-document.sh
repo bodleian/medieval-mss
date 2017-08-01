0a1d235b97d33ec359eb50c2911e42896e8c2e2f
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-08-01 09:54:27 +0100

    Fixed: Update solr document generation script

--- processing/generate-solr-document.sh
+++ processing/generate-solr-document.sh
@@ -9,8 +9,12 @@
 echo "Generating Solr XML document"
 java -cp "saxon/saxon9he.jar" net.sf.saxon.Query -q:$1 | xmllint --format - > $2
 
-echo "Emptying Solr"
-curl "http://${4}:8983/solr/tolkien-catalogue/update?stream.body=<delete><query>type:${3}</query></delete>&commit=true"
+# Emptying for both place and organization will result in only one of them being indexed; if we're indexing
+# organizations, then, skip the empty step.
+if [ ! $1 == "organizations.xquery" ]; then
+    echo "Emptying Solr"
+    curl "http://${4}:8983/solr/medieval-mss/update?stream.body=<delete><query>type:${3}</query></delete>&commit=true"
+fi
 
 echo "Reindexing in Solr"
-curl "http://${4}:8983/solr/tolkien-catalogue/update?commit=true" --data-binary @$2 -H "Content-Type: text/xml"
+curl "http://${4}:8983/solr/medieval-mss/update?commit=true" --data-binary @$2 -H "Content-Type: text/xml"
