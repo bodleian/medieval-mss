ce0ea41203023b0161f94724738a1440181dd289
Andrew Morrison <andrew.morrison@bodleian.ox.ac.uk>
2017-12-04 17:21:27 +0000

    Fixing conflict on processing/index-all-qa.sh

--- processing/index-all-qa.sh
+++ processing/index-all-qa.sh
@@ -13,17 +13,19 @@ fi
 cd "${0%/*}"
 
 # Re-index manuscripts (includes rebuilding customized manuscript HTML pages, which must be run first)
-./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-prd.bodleian.ox.ac.uk $1
+./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-qa.bodleian.ox.ac.uk $1
 
 # Reindex places (includes organizations, which must be run second)
-./generate-solr-document.sh places.xquery places_index.xml place solr01-prd.bodleian.ox.ac.uk $1
+./generate-solr-document.sh places.xquery places_index.xml place solr01-qa.bodleian.ox.ac.uk $1
 if [ ! "$1" == "noindex" ]; then
     echo "Place index will be incomplete until organizations have also been reindexed."
 fi
-./generate-solr-document.sh organizations.xquery organizations_index.xml organization solr01-prd.bodleian.ox.ac.uk $1
+./generate-solr-document.sh organizations.xquery organizations_index.xml organization solr01-qa.bodleian.ox.ac.uk $1
 
 # Reindex people
-./generate-solr-document.sh people.xquery people_index.xml person solr01-prd.bodleian.ox.ac.uk $1
+./generate-solr-document.sh people.xquery people_index.xml person solr01-qa.bodleian.ox.ac.uk $1
 
 # Reindex works
-./generate-solr-document.sh works.xquery works_index.xml work solr01-prd.bodleian.ox.ac.uk $1
+./generate-solr-document.sh works.xquery works_index.xml work solr01-qa.bodleian.ox.ac.uk $1
+
+
