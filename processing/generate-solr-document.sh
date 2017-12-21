f0dae47f4c4eac77dffd4fa5c3f66c86b3f12523
Andrew Morrison <andrew.morrison@bodleian.ox.ac.uk>
2017-12-21 16:00:41 +0000

    Script tweaks copied from Fihrist version

--- processing/generate-solr-document.sh
+++ processing/generate-solr-document.sh
@@ -26,36 +26,39 @@ fi
 
 # Start log file
 LOGFILE="solr/$3.log"
-echo "Processing TEI files in collections folder using $1 on $(date +"%Y-%m-%d %H:%M:%S") to be sent to $4 for reindexing." > $LOGFILE
+echo "Processing TEI files in collections folder using $1 on $(date +"%Y-%m-%d %H:%M:%S") to be sent to $4 for re-indexing." > $LOGFILE
 
 # Run XQuery to build Solr XML index files
-echo "Generating XML file to send to Solr for $3s..."
+echo "Generating Solr XML file containing $3 records..."
 java -Xmx1G -Xms1G -cp "saxon/saxon9he.jar" net.sf.saxon.Query -q:$1 1> solr/$2 2>> $LOGFILE
 if [ $? -gt 0 ]; then
-    echo "XQuery failed. Please raise an issue on GitHub, attaching $LOGFILE. Reindexing of $3 cancelled."
+    echo "XQuery failed. Re-indexing of $3 records cancelled. Please raise an issue on GitHub, attaching $LOGFILE"
     exit 1;
 fi
 
 # Clean up log file (because XQuery/Saxon appends some junk to the end of each line)
-rev $LOGFILE | cut -f 2- | rev > $LOGFILE.tmp && mv $LOGFILE.tmp $LOGFILE
+# Doesn't work in git-bash which lacks the rev command
+if hash rev 2>/dev/null; then
+    rev $LOGFILE | cut -f 2- | rev > $LOGFILE.tmp && mv $LOGFILE.tmp $LOGFILE
+fi
 
 # Check what's been logged
 errors=$(grep -ic "^error" $LOGFILE)
 if [ $errors -gt 0 ]; then
-    echo "There are $errors error messages in $LOGFILE. Reindexing of $3 cancelled."
+    echo "There are $errors error messages in $LOGFILE so re-indexing of $3 records cannot proceed."
     exit 1;
 fi
 warnings=$(grep -ic "^warn" $LOGFILE)
 infos=$(grep -ic "^info" $LOGFILE)
 if [ $warnings -gt 0 ] || [ $infos -gt 0 ]; then
-    echo "There are $warnings warning and $infos info messages in $LOGFILE."
+    echo "There are $warnings warning and $infos info messages in $LOGFILE"
     if [ ! "$5" == "force" ] && [ ! "$5" == "noindex" ]; then
         while true; do
-            read -p "Do you wish to reindex? [Yes|No|Quit|View] " answer
+            read -p "Do you wish to rebuild the $3 index? [Yes|No|Quit|View] " answer
             case $answer in
                 [Yy]|YES|Yes|yes ) break;;
-                [Nn]|NO|No|no ) echo "Reindexing of $3 cancelled. Proceeding to next index."; exit 0;;
-                [Qq]|QUIT|Quit|quit ) echo "Reindexing of $3 cancelled. Abandoning all further indexing."; exit 1;;
+                [Nn]|NO|No|no ) echo "Re-indexing of $3 records cancelled. Proceeding to next index."; exit 0;;
+                [Qq]|QUIT|Quit|quit ) echo "Re-indexing of $3 records cancelled. Abandoning all further indexing."; exit 1;;
                 [Vv]|VIEW|View|view ) less $LOGFILE; echo;;
                 * ) echo;;
             esac
@@ -77,18 +80,18 @@ if [ ! "$5" == "noindex" ]; then
         echo "Emptying Solr failed. Try again later. If problem persists, please raise an issue on GitHub, attaching $LOGFILE."
         exit 1;
     else
-        echo "Reindexing $3 in Solr..."
+        echo "Sending new $3 records to Solr..."
         curl -fsS "http://${4}:8983/solr/medieval-mss/update?commit=true" --data-binary @solr/$2 -H "Content-Type: text/xml" 1>> $LOGFILE 2>> $LOGFILE
         if [ $? -eq 0 ]; then
-            echo "Reindexing of $3 finished. Please check the web site for expected changes."
+            echo "Re-indexing of $3 records finished. Please check the web site for expected changes."
             exit 0;
         else
-            echo "Reindexing of $3 failed. The web site will have no $3s. If this is on production, raise an urgent issue on GitHub, attaching $LOGFILE."
+            echo "Re-indexing of $3 records failed. The web site will have no $3s. If this is on production, raise an urgent issue on GitHub, attaching $LOGFILE."
             exit 1;
         fi
     fi
 else
-    echo "Reindexing skipped in $5 mode."
+    echo "Re-indexing skipped in $5 mode."
     exit 0;
 fi
 
