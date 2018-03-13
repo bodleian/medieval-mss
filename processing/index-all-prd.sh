60f737705c1d92ce836d940f41899c26bf49819d
holfordm <Matthew.Holford@bodleian.ox.ac.uk>
2018-03-13 18:09:32 +0000

    minor updates

--- processing/index-all-prd.sh
+++ processing/index-all-prd.sh
@@ -13,7 +13,8 @@ fi
 cd "${0%/*}"
 
 # Re-index manuscripts (includes rebuilding customized manuscript HTML pages, which must be run first)
-./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-prd.bodleian.ox.ac.uk $1
+#./generate-html.sh 
+./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-prd.bodleian.ox.ac.uk $1
 
 # Reindex places (includes organizations, which must be run second)
 ./generate-solr-document.sh places.xquery places_index.xml place solr01-prd.bodleian.ox.ac.uk $1
