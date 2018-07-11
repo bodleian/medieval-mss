23953da596db8f03fa4cb637b8a450683b9f229d
Andrew Morrison <andrew.morrison@bodleian.ox.ac.uk>
2018-07-11 15:10:02 +0100

    Fix: Exit if XSLT fails
    For example, if no xml:id has been given to a manuscript's root TEI element.

--- processing/generate-html.sh
+++ processing/generate-html.sh
@@ -3,8 +3,10 @@
 echo
 echo "Generating HTML for customized manuscript view..."
 
-# Change directory to the location of this script
-cd "${0%/*}"
+if [[ ! "`pwd`" == *-mss/processing ]]; then
+    echo "This script must be run from the processing folder"
+    exit 1
+fi 
 
 # Create subfolder to keep generated files out of GitHub
 if [ ! -d "html" ]; then
@@ -17,3 +19,7 @@ echo "Transforming TEI files in collections folder using convert2HTML.xsl on $(d
 
 # Run XSLT on all TEI files in collections path (using pwd to get full path, not relative, which is what the XSL needs)
 java -Xmx1G -Xms1G -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:batch -xsl:convert2HTML.xsl collections-path=`pwd`/../collections/ 2>> $LOGFILE
+if [ $? -gt 0 ]; then
+    echo "XSLT failed. Re-indexing cancelled. Please raise an issue on GitHub, attaching $LOGFILE"
+    exit 1;
+fi
