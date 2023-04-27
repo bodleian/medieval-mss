6bc65703e5afdc5d708dbfd2bff71fbba8288b62
Andrew Dunning <adunning@users.noreply.github.com>
2023-04-27 17:32:17 +0100

    Improve documentation

--- processing/add_work_subjects.py
+++ processing/add_work_subjects.py
@@ -5,6 +5,9 @@ Prompts the user to select a subject classification
 from <category> elements defined in `works.xml`.
 
 The selected subject is added as a child <term> to the <bibl> element.
+
+Run from the main project directory:
+    $ python3 processing/add_work_subjects.py
 """
 
 from lxml import etree
@@ -12,13 +15,18 @@ from lxml import etree
 # define XML namespaces
 NS: dict[str, str] = {"tei": "http://www.tei-c.org/ns/1.0"}
 
-WORKS_FILE = "works.xml"
+WORKS_FILE: str = "works.xml"
 
 
 def get_categories(tree: etree.ElementTree) -> dict[str, str]:
-    """Create a dictionary of category IDs and descriptions from the works file."""
+    """
+    Create a dictionary of categories from the works file.
+
+    Returns a dictionary of category IDs and descriptions.
+        Key: `@xml:id` of the category element
+        Value: text of the <catDesc> element
+    """
     return {
-        # get the xml:id of each category element with the text of the <catDesc> element
         category.get("{http://www.w3.org/XML/1998/namespace}id"): category.findtext(
             "tei:catDesc", namespaces=NS
         )
@@ -30,47 +38,49 @@ def add_subject(
     works_tree: etree.ElementTree, bibl: etree.Element, categories: dict[str, str]
 ) -> None:
     """
-    Prompt the user to select a category, add it as a <term> to the <bibl> element,
-    and write the XML.
+    Prompt the user to select a category. Add it as a <term> to the <bibl> element.
     """
     print("\nWork without a subject:\n")
+    # print the title of the work
     print(
         f"{', '.join(title.text for title in bibl.findall('tei:title', namespaces=NS))}"
     )
 
     print("\nAvailable categories:")
-    category_numbers = []
-    for i, category in enumerate(categories):
-        if i % 3 == 0:
+    # print the categories in three columns
+    category_numbers: list[str] = []
+    for index, category in enumerate(categories):
+        if index % 3 == 0:
             print()
-        print(f"{i+1}. {categories[category]:<25}", end="")
-        category_numbers.append(str(i + 1))
+        print(f"{index+1}. {categories[category]:<25}", end="")
+        category_numbers.append(str(index + 1))
 
     while True:
-        category_indices = input(
+        category_selections: list[str] = input(
             "\nEnter category number(s), separated by spaces: "
         ).split()
 
         # if entry is blank, skip this <bibl>
-        if not category_indices:
+        if not category_selections:
             print("\nSkipping this work.")
             break
 
         # if entry is invalid, prompt again
         if all(
-            index.isdigit() and index in category_numbers for index in category_indices
+            index.isdigit() and index in category_numbers
+            for index in category_selections
         ):
             break
         else:
             print("\nInvalid input. Please enter valid category number(s).\n")
 
-    selected_categories = [
+    selected_category_indices: list[str] = [
         list(categories.keys())[int(index) - 1]
-        for index in category_indices
+        for index in category_selections
         if index.isdigit()
     ]
 
-    for selected_category in selected_categories:
+    for selected_category in selected_category_indices:
         bibl.append(etree.Element("term", ref=f"#{selected_category}", nsmap=NS))
 
     write_xml(works_tree, WORKS_FILE)
@@ -89,9 +99,9 @@ def write_xml(tree: etree.ElementTree, file_path: str) -> None:
 
 
 def fix_xml_declaration(file_path: str) -> None:
-    """Fix XML declaration reformatting from lxml"""
+    """Fix XML declaration reformatting from lxml."""
     with open(file_path, "r", encoding="utf-8") as file:
-        processed_xml = file.read().replace(
+        processed_xml: str = file.read().replace(
             "<?xml version='1.0' encoding='UTF-8'?>",
             '<?xml version="1.0" encoding="UTF-8"?>',
         )
